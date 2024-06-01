2e0f622564486dc7c1eeb1a5eaa4da24fc0153b4
Fábio Augusto Romanini Pinto <fabioaromanini@gmail.com>
2024-06-01 11:59:44 -0300

    changes default application por to 8080

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@ import app from "./app";
 
 function main() {
   try {
-    app.listen(3000, "localhost", () => {
-      console.log("Server running at port 3000");
+    app.listen(8080, "localhost", () => {
+      console.log("Server running at port 8080");
     });
   } catch (error) {
     console.log("Error: ", error);
