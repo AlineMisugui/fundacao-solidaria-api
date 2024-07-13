b95d8fd94a7278d25586398f0237387a41f897f6
Fábio Augusto Romanini Pinto <fabioaromanini@gmail.com>
2024-07-13 11:28:40 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import app from "./app";
 function main() {
   try {
     app.listen(8080, () => {
-      console.log("Server running at port 8080");
+      console.log("Server running at port 8080!!!");
     });
   } catch (error) {
     console.log("Error: ", error);
